d167bb7d536742ee00c7c6ddb1d1a2f8ba26cc95
Brendan Morrell <brendanmorrell@gmail.com>
2020-03-18 17:47:33 -0700

    example repo set up

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// EXAMPLE
+// highlighted sections will all display the following error:
+
+// Visible, non-interactive elements with click handlers must have at least one keyboard listener.
+// eslint(styled-components-a11y/click-events-have-key-events) */
+
+const Div = styled.div``;
+
+// div with no onclick does not have on error
+const DivNoError = () => <Div />;
+// when onClick is added, error appears
+const DivError = () => <Div onClick={() => 0} />;
+
+// props can be added via attrs method
+const DivAttrs = styled.div.attrs({ foo: 'bar' })``;
+const DivAttrsNoError = () => <DivAttrsNoError />;
+
+// if those props trigger an error, this trickles down to the component
+const DivOnClickAttrs = styled.div.attrs({ onClick: () => 0 })``;
+const DivOnClickAttrsError = () => <DivOnClickAttrs />;
+
+// buttons can have click handlers (aria label added to silence 'control-has-associated-label'error.
+// it is kept on bottom example just to show it is not the source of the error when using as="div")
+const ButtonOnClickAttrs = styled.button.attrs({ onClick: () => 0, 'aria-label': 'Save' })``;
+const ButtonOnClickAttrsNoError = () => <ButtonOnClickAttrs />;
+// when this component is rendered as a div, the click-events-have-key-events error resurfaces
+const ButtonOnClickAttrsAsDiv = styled.button.attrs({ onClick: () => 0, 'aria-label': 'Save' })``;
+const ButtonOnClickAttrsAsDivError = () => <ButtonOnClickAttrsAsDiv as="div" />;
+
+export default {
+  DivNoError,
+  DivError,
+  DivAttrs,
+  DivOnClickAttrsError,
+  ButtonOnClickAttrsNoError,
+  ButtonOnClickAttrsAsDivError,
+};
